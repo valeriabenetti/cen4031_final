cfdf0a29566bedf15817d6e7cceee74a81e2c56b
Val <vbenetti46@gmail.com>
2024-10-03 18:32:42 -0400

    Updating server.py to allow application layer usability

--- migrations/server.py
+++ migrations/server.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
-
+from sqlalchemy.orm import sessionmaker
+from Data_access_layer.Repository import TicketRepo
 from api import router
 
-def create_server(session_maker=none, repositories=none):
+def create_server(session_maker: sessionmaker = None, repositories=None):
     server = FastAPI(debug=True)
     server.include_router(router)
+    
+    if session_maker is None:
+        raise ValueError("session_maker must be provided")
+    
     server.session_maker = session_maker
+    
+    if repositories is None:
+        # Initialize repositories if not provided
+        repositories = {
+            "ticketing_repo": lambda session: TicketRepo(session)
+        }
+    
     server.repositories = repositories
+    
     return server
